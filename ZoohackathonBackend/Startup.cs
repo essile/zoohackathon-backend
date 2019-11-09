f9f93011bf1a6ab64cd4669df51555a22b49a790
esle <essi.lehtola@hotmail.com>
2019-11-09 12:16:54 +0200

    Added Swagger

--- ZoohackathonBackend/Startup.cs
+++ ZoohackathonBackend/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ZoohackathonBackend
 {
@@ -26,6 +27,11 @@ namespace ZoohackathonBackend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Zoohackathon Backend", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +45,12 @@ namespace ZoohackathonBackend
             {
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Zoohackathon backend V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
